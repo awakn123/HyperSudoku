a4c002ad351fd6cacb2e1bbba34e632c763cd42f
LiyangSong <song.liy@northeastern.edu>
2022-12-11 17:17:03 -0500

    layout update

--- src/SudokuView.js
+++ src/SudokuView.js
@@ -29,29 +29,6 @@ const SudokuView = () => {
 
   return (
       <div className={'sudoku-view'}>
-        <div className="header">
-          <div className="logo">
-            <img src={logo} alt="NEU logo" height="60px" width="60px"/>
-          </div>
-
-          <div className="title">
-            Hyper Sudoku Solver
-          </div>
-
-          <div className="placeholder"></div>
-
-          <div className="gitlogo">
-            <a href="https://github.com/awakn123/HyperSudoku" target="_blank">
-              <img src={GithubLogo} alt="Git logo" height="30px" width="30px"/>
-            </a>
-          </div>
-
-          <div className="gitlink">
-            <a href="https://github.com/awakn123/HyperSudoku" target="_blank">
-              Project Page on Github
-            </a>
-          </div>
-        </div>
 
         <div className="content">
           <div className="column left">
@@ -95,22 +72,6 @@ const SudokuView = () => {
                 {logs.map((log, idx) => <li key={idx}>{log}</li>)}
               </ul>
             </div>
-          </div>
-        </div>
-
-        <div className="footer">
-          <div className="course">
-            CS5800 Final Project
-          </div>
-
-          <div className="semester">
-            2022 Fall Semester
-          </div>
-
-          <div className="placeholder"></div>
-
-          <div className="author">
-            Author (Alphabetical Order): Liyang Song, Na Yin, Xueyan Feng, Yun Cao
           </div>
         </div>
       </div>
